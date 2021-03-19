90227fa813ebf2f810eea7577f76ecbc22657ace
yedibela-mms <yedibela@mediamarktsaturn.com>
2021-03-19 13:04:58 +0100

    Initial Spring Boot Webclient example

---
+++ Spring-Boot-Webclient/src/test/java/learning/java/spring/boot/webclient/WebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.java.spring.boot.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
